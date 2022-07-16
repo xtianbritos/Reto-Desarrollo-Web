0305b0b22d99cfed84214cec16e8c3fb3eb687b7
Xtian <xtiancuenta1@gmail.com>
2022-07-15 23:46:01 -0300

    Entrega

--- Back-reto/src/main/java/com/sofka/Software/controller/ListTaskController.java
+++ Back-reto/src/main/java/com/sofka/Software/controller/ListTaskController.java
@@ -4,7 +4,6 @@ import com.sofka.Software.models.ListTaskModel;
 import com.sofka.Software.services.ListTaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
